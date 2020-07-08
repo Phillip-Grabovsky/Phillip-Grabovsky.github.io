003def485efca39c18d68b6f722d3a67a5fe8524
Phillip-Grabovsky <phillip.grabovsky@gmail.com>
2020-07-08 14:45:58 -0400

    Randomize on interval

--- blocks.js
+++ blocks.js
@@ -1,15 +1,18 @@
-var numberBoxes = 37.5; //constant: number of boxes that should fit in one width
+var numberBoxes = 45; //constant: number of boxes that should fit in one width
 var maxHeight = 3; //max number of boxes stacked
 var width = screen.width;
 var boxWidth = (width / numberBoxes) - 2;
 var divNumber = 0;
+var ongoingInterval;  //this is the global variable which holds the timed randomization process inside it.
+var playing = 0; //whether the timed re-randomization is playing or not.
+randomizeBoxes();
 
 function randomizeBoxes(){
   for(var i = 1; i < numberBoxes+1; i++){
     var height = Math.floor(Math.random() * (maxHeight+1));
     createBoxes(i, height);
   }
-  createBoxes(0,1);
+  createBoxes(0,2);
 }
 
 function createBoxes(col, height){
@@ -19,28 +22,55 @@ function createBoxes(col, height){
 }
 
 function createBox(col, row){
-  //if its the final box on the right, make it a button that lets you re-randomize it or play video!
+
+  //if it's one of the buttons on the left, make it a button that lets you re-randomize it or play video!
   if(col == 0){
-    var newBox = document.createElement("button");
-    newBox.type = "button"
+    var node; //node is initialized here because the button's text changes on stop/start
+    var newBox = document.createElement("div");
+
     //lower button re-randomizes
     if(row == 0){
+      node = document.createTextNode("o");
       newBox.addEventListener("click", function(){restart();});
-      //style the button
-      var text = document.createElement("p");
-      var node = document.createTextNode("o");
-      text.style.textAlign = "center";
-      text.style.margin = "auto 0 auto 0";
-      text.style.color = "#2F4F4F";
-      text.appendChild(node);
-      newBox.appendChild(text);
     }
+
+    //upper button re-randomizes on time interval.
+    else if(row == 1){
+      //if it's not playing, the button's function is to play
+      if(playing == 0){
+        node = document.createTextNode(">>");
+        newBox.addEventListener("click", function(){
+          ongoingInterval = setInterval(restart, 200);
+          playing = 1;
+        });
+      }
+      //if it is playing, the button's function is to stop playing.
+      else if(playing == 1){
+        node = document.createTextNode("| |");
+        newBox.addEventListener("click", function(){
+          clearInterval(ongoingInterval);
+          playing = 0;
+          restart();
+        });
+      }
+    }
+    //style the button
+    newBox.style.cursor = "pointer";
+    var text = document.createElement("p");
+    text.style.textAlign = "center";
+    text.style.margin = "auto 0 auto 0";
+    text.style.color = "#2F4F4F";
+    text.appendChild(node);
+    newBox.appendChild(text);
   }
 
+  //else: it's not one of the buttons
   else{
     //make the div if its not the final one.
     var newBox = document.createElement("div");
   }
+
+  //give it the correct position
   var left = col*boxWidth;
   var top = (-1)*(row+1)*boxWidth - boxWidth*divNumber;  //2nd term accounts for downshift in starting points of divs
 
@@ -73,10 +103,3 @@ function restart(){
   //build new boxes!
   randomizeBoxes();
 }
-
-function randomizeBoxesInterval(){
-  setInterval(restart, 500);
-}
-
-
-randomizeBoxesInterval();
